09ba01e3ad117c1d4a3c28d2739a201c5166301a
Bart <liempdbart@gmail.com>
2024-08-12 00:18:41 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.getElementById("playerForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevents the form from submitting
+    generateInputs(); // Call your function
+});
+
+const generateInputs = function(){
+    console.log("test")
+}
+
+
