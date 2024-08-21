a782a6f236946b240f53b1821b9be92c147f4e5e
Bart <liempdbart@gmail.com>
2024-08-22 00:16:51 +0200

    HTML aanpassingen

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 
 document.getElementById("playerForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevents the form from submitting
-    generateInputs(); // Call your function
+    generateInputs("playerForm"); // Call your function
 });
 
-const generateInputs = function(){
-    console.log("test")
+const generateInputs = function(ele){
+    let aantalSpelers = document.getElementById(ele)[0].value
+
+    while (parseInt(aantalSpelers) > 0){
+
+
+        // Create a div to wrap each input field
+        const playerInputContainer = document.createElement("div");
+
+
+        const playerName = document.createElement("input");
+        playerName.type = "text";
+        playerName.name = `playername${aantalSpelers}`;
+        playerName.width = 100%
+
+        playerInputContainer.appendChild(playerName);
+
+        document.getElementById("playerNames").append(playerInputContainer)
+        aantalSpelers = parseInt(aantalSpelers) -1;
+    }
 }
 
 
