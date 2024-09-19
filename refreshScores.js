0c39a9fd3950bbff36527bcbe7df041e1bee6b4b
Bart <liempdbart@gmail.com>
2024-09-19 12:10:15 +0200

    Added base for refreshing scores

--- refreshScores.js
+++ refreshScores.js
@@ -1,5 +1,14 @@
 function refreshScores(){
-    console.log(JSON.parse(localStorage["gameInformation"]))
+    let currentInformation = JSON.parse(localStorage["gameInformation"])
 
     // Get current round again from api and update everything accordingly
+    getTeams("https://api.keukenkampioendivisie.nl/wp-json/statsperform/v1/round-schedule")
+  .then(teams => {
+    // match teams and currentinformation
+    currentInformation.forEach(player => {
+        const teams = player.teams.map(t => t.teamName)
+        console.log(teams)
+    })
+
+  })
 }
